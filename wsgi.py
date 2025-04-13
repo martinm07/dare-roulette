7547e7c5adbc8564f4037a0d79f736428a56ae0f
martinm07 <martin.github07@gmail.com>
2025-04-14 00:12:48 +0100

    Add print statement for DATABASE_URI in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,10 @@
+import os
+
 from dotenv import load_dotenv
 
 from flaskapp import create_app
 
 load_dotenv()
 
+print("DATABASE_URI:", os.environ.get("SQLALCHEMY_DATABASE_URI"))
 app = create_app()
