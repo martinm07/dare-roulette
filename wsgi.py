0853b832d8ea597020e9a0a5e6276a3265a3e7f8
martinm07 <martin.github07@gmail.com>
2025-04-18 00:04:19 +0100

    Try pure python mysql client instead

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,13 @@
 import os
 
+import pymysql
 from dotenv import load_dotenv
 
 from flaskapp import create_app
 
 load_dotenv()
+
+pymysql.install_as_MySQLdb()
 
 print("DATABASE_URI:", os.environ.get("SQLALCHEMY_DATABASE_URI"))
 app = create_app()
